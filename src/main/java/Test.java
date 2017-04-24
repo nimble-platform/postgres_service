d67e7cc65ee2e0d4263c0d4b9cb9f729e7175663
Evgeniy <evgenih@il.ibm.com>
2017-04-24 15:17:27 +0300

    initial development

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -3,16 +3,5 @@
  */
 public class Test {
     public static void main(String[] args) {
-
-        System.out.println(uri.getHost());
-        System.out.println(uri.getUserInfo());
-        System.out.println(uri.getAuthority());
-        System.out.println(uri.getPort());
-
-        String[] credentials = uri.getRawUserInfo().split(":");
-        System.out.println(credentials[0]);
-        System.out.println(credentials[1]);
-        
-
     }
 }
